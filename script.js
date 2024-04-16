540c0b94248ec9d8df880048e16fc96aff969183
Sushovan <sushovanghosh960@gmail.com>
2024-04-16 21:39:52 +0530

    refactor: refactor code to avoid error

--- script.js
+++ script.js
@@ -2,26 +2,32 @@ import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
 
 const history = [];
+const inc = 'increment', dec = 'decrement', incByAmt = 'incrementByAmt';
+
+
 const store = createStore(reducer, applyMiddleware(logger.default));
 
 function reducer(state = { amount: 1 }, action) {
 
-    if (action.type === 'increment') {
+    if (action.type === inc) {
         // redux doesn't allow to mutate the orginal state
         // state.amount++;
 
         return { amount: state.amount + 1 } // You need to make a copy of the state.
     }
 
-    if (action.type === 'decrement') return { amount: state.amount - 1 };
+    if (action.type === dec) return { amount: state.amount - 1 };
 
-    if (action.type === 'incrementByAmt') return { amount: state.amount + action.payload };
-
+    if (action.type === incByAmt) return { amount: state.amount + action.payload };
 
     return state;
 }
 
+// Action creators
+const increment = () => ({ type: 'increment' });
+const incrementByAmt = () => ({ type: 'incrementByAmt', payload: 5 });
+const decrement = () => ({ type: 'decrement' });
+
 setInterval(() => {
-    store.dispatch({ type: 'decrement' });
-    store.dispatch({ type: 'incrementByAmt', payload: 4 });
+    store.dispatch(incrementByAmt());
 }, 2000);
