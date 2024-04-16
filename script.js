c2854ffa0834f3a4a3c1a372af279ef862c0b4ca
Sushovan <sushovanghosh960@gmail.com>
2024-04-16 21:11:50 +0530

    add applyMiddleware to log every action in in store
    use redux-logger to log every change of state in the store

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
 
+const history = [];
+const store = createStore(reducer, applyMiddleware(logger.default));
 
-const store = createStore(function (state = { amount: 1 }, action) {
+function reducer(state = { amount: 1 }, action) {
+
+    if (action.type === 'increment') {
+        // redux doesn't allow to mutate the orginal state
+        // state.amount++;
+
+        return { amount: state.amount + 1 } // You need to make a copy of the state.
+    }
+
     return state;
-});
-console.log(store.getState());
+}
 
+setInterval(() => {
+    store.dispatch({ type: 'increment' });
+}, 2000);
