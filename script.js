f037dcb544a1dcf25c9a4c9af912ffefac79af15
Sushovan <sushovanghosh960@gmail.com>
2024-04-17 09:24:22 +0530

    add redux-thunk as a middleware to stop dispatcher temporarily
    thunk acts as a middlware and it stops any asynchronous operation without updating the state
    refactor code with switch statement instead of if..else
    install axios to call an API asynchronously

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
 import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
+import axios from "axios";
+import { thunk } from "redux-thunk";
 
 const history = [];
-const inc = 'increment', dec = 'decrement', incByAmt = 'incrementByAmt';
+const inc = 'increment', dec = 'decrement', incByAmt = 'incrementByAmt', init = 'initial';
 
 
-const store = createStore(reducer, applyMiddleware(logger.default));
+const store = createStore(reducer, applyMiddleware(logger.default, thunk));
 
 function reducer(state = { amount: 1 }, action) {
 
-    if (action.type === inc) {
-        // redux doesn't allow to mutate the orginal state
-        // state.amount++;
-
-        return { amount: state.amount + 1 } // You need to make a copy of the state.
+    switch (action.type) {
+        case init:
+            return { amount: action.payload }
+        case inc:
+            return { amount: state.amount + 1 };
+        case dec:
+            return { amount: state.amount - 1 };
+        case incByAmt:
+            return { amount: state.amount + action.payload }
+        default:
+            return state;
     }
-
-    if (action.type === dec) return { amount: state.amount - 1 };
-
-    if (action.type === incByAmt) return { amount: state.amount + action.payload };
-
-    return state;
 }
 
 // Action creators
-const increment = () => ({ type: 'increment' });
-const incrementByAmt = () => ({ type: 'incrementByAmt', payload: 5 });
-const decrement = () => ({ type: 'decrement' });
+const increment = () => ({ type: inc });
+const incrementByAmt = () => ({ type: incByAmt, payload: 5 });
+const decrement = () => ({ type: dec });
+async function getUser(dispatch, getState) {
+    const { data } = await axios.get('http://localhost:3000/accounts/1');
+    dispatch(initUser(data.amount));
+};
+const initUser = (value) => ({ type: init, payload: value });
 
-setInterval(() => {
-    store.dispatch(incrementByAmt());
+
+setTimeout(() => {
+    // store.dispatch(incrementByAmt());
+    store.dispatch(getUser);
 }, 2000);
